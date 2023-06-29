541846ec61515982f55c6a1406a8f9c5d1c93e7b
Ghostyshka <denis2018steam@gmail.com>
2023-06-29 21:25:28 +0300

    refactor for vntu

--- MailApp/MailApp.Core/Models/RegexConstants.cs
+++ MailApp/MailApp.Core/Models/RegexConstants.cs
@@ -10,7 +10,7 @@ namespace MailApp.Core.Models
         public static readonly Regex AuthorRegex = new Regex("(Автор: ?)(.*)(\\n)");
         public static readonly Regex TopicRegex = new Regex("(Тема: ?)(.*)(\\n)");
         public static readonly Regex PagesRegex = new Regex("(Сторінки: ?)(.*)(\\n)");
-        public static readonly Regex ReviewersRegex = new Regex("(Рецензенти: ?)(.*)(\\n)");
+        public static readonly Regex ReviewersRegex = new Regex("(Рецензенти: ?)((\\n|.)*$)");
 
         public static readonly Regex FullNameRegex = new Regex("(ПІБи: ?)(.*)(\\n)");
         public static readonly Regex TitleRegex = new Regex("(Назва роботи: ?)(.*)(\\n)");
